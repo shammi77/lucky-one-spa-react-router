391badd9b87917c33b38eb87a834c2ffe85aaff7
shammi77 <nusratshammi.vutu@gmail.com>
2022-03-27 13:43:58 +0600

    ffirst commit

---
+++ src/component/jewellery/Jewellery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Jewellery = () => {
+    return (
+        <div>
+            <h2>jewellery</h2>
+            <div className="jewellery-item"></div>
+            <div className="selected-jewellery"></div>
+        </div>
+    );
+};
+
+export default Jewellery;
